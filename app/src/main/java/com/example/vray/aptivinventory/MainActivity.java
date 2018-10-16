f32b32c42af98a551ccba73ada7ea61a0e7431a0
vraykilla <vray25.vl@gmail.com>
2018-10-16 01:57:48 -0600

    Login Design

---
+++ app/src/main/java/com/example/vray/aptivinventory/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.vray.aptivinventory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+    }
+}
