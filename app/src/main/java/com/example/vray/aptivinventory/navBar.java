dbabf6e85c2d46da7757bf7fc6d38c8dfba28c98
vray22 <vray25.vl@gmail.com>
2018-11-07 12:22:02 -0700

    NavBar

---
+++ app/src/main/java/com/example/vray/aptivinventory/navBar.java
@@ -0,0 +1,13 @@
+package com.example.vray.aptivinventory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class navBar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav_bar);
+    }
+}
